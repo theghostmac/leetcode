a7d474dbceab38a28336798d6803c1c15f8876c0
theghostmac <theghostmac@gmail.com>
2024-01-02 05:55:31 +0100

    finished solving longest palindromic substring problem in an entire dayyyy

--- daytwo/long-palindromic.go
+++ daytwo/long-palindromic.go
@@ -1,17 +1,38 @@
 package daytwo
 
-// given a string s
-// return the longest palindromic substring.
-
 func longestPalindrome(s string) string {
-	n := len(s)
-	start, maxLength := 0, 1
+	// iterate through the string.
+	// check for potential longest palindromic centers.
+	// expand for odd-length and even-length cases.
+	// move left and right from the center to find the longest.
+	// store entries of the longest found palindromic substring into a variable.
+	stringLength := len(s)
+	start := 0
+	maxLength := 1
 
-	// iterate through each character of s as potential center of the palindrome.
-	for i := 0; i < n; i++ {
-		left, right := i, i
-		for left >= 0 && right < n && s[left] == s[right] {
-			
+	for character := 0; character < stringLength; character++ {
+		// case of odd-length center
+		leftChar, rightChar := character, character
+		// when a palindrome is found
+		for leftChar >= 0 && rightChar < stringLength && s[leftChar] == s[rightChar] {
+			// if a longer palindrome is found, track it instead of existing one.
+			newPalindrome := rightChar - leftChar + 1
+			if newPalindrome > maxLength {
+				start = leftChar
+				maxLength = newPalindrome
+			}
+		}
+
+		// case of even-length center
+		leftChar, rightChar = character, character+1
+		for leftChar >= 0 && rightChar < stringLength && s[leftChar] == s[rightChar] {
+			newPalindrome := rightChar - leftChar + 1
+			if newPalindrome > maxLength {
+				start = leftChar
+				maxLength = newPalindrome
+			}
 		}
 	}
+
+	return s[start : start+maxLength]
 }
